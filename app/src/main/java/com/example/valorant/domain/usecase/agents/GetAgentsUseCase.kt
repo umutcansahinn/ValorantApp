b5e692384db4f8c38979c9e6bc2b58bcb971ccf6
Umutcan Sahin <umutcan199726@gmail.com>
2023-03-01 17:40:23 +0300

    added getAgentsUseCase

---
+++ app/src/main/java/com/example/valorant/domain/usecase/agents/GetAgentsUseCase.kt
@@ -0,0 +1,34 @@
+package com.example.valorant.domain.usecase.agents
+
+import com.example.valorant.common.Resource
+import com.example.valorant.domain.mapper.AgentMapper
+import com.example.valorant.domain.model.Agent
+import com.example.valorant.domain.repository.ValorantRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetAgentsUseCase @Inject constructor(
+    private val valorantRepository: ValorantRepository,
+    private val mapper: AgentMapper
+) {
+
+    operator fun invoke(): Flow<Resource<List<Agent>>> {
+        return flow {
+            try {
+                emit(Resource.Loading)
+                valorantRepository.getAgents().data?.map {
+                    mapper.map(agentData = it)
+                }?.let {
+                    emit(Resource.Success(data = it))
+                }
+            }catch (e: HttpException) {
+                emit(Resource.Error(e.localizedMessage.orEmpty()))
+            }catch (e: IOException) {
+                emit(Resource.Error(e.localizedMessage.orEmpty()))
+            }
+        }
+    }
+}
