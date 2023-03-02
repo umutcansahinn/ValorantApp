12d787eaec1488d5a7ca93874f0bc53a1b3e4cc8
Umutcan Sahin <umutcan199726@gmail.com>
2023-03-02 13:37:59 +0300

    update glide

--- app/src/main/java/com/example/valorant/common/ImageViewExt.kt
+++ app/src/main/java/com/example/valorant/common/ImageViewExt.kt
@@ -2,9 +2,13 @@ package com.example.valorant.common
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.example.valorant.R
 
 fun ImageView.loadImage(url: String?) {
         Glide.with(this)
             .load(url)
+            .centerCrop()
+            .circleCrop()
+            .placeholder(R.drawable.baseline_cached_24)
             .into(this)
     }
