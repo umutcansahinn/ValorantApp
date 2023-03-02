fe37ee984c1862505e925329274192aa4eb1a4f8
Umutcan Sahin <umutcan199726@gmail.com>
2023-03-02 14:49:05 +0300

    changed recyclerview manager

--- app/src/main/java/com/example/valorant/ui/agent/agents/AgentsFragment.kt
+++ app/src/main/java/com/example/valorant/ui/agent/agents/AgentsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.valorant.R
 import com.example.valorant.common.gone
@@ -58,7 +59,7 @@ class AgentsFragment : Fragment(R.layout.fragment_agents) {
     }
     private fun initView() {
         binding.recyclerView.adapter = agentsAdapter
-        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.layoutManager = GridLayoutManager(requireContext(),3)
     }
 
     private fun itemSetClick(agentUuid: String) {
